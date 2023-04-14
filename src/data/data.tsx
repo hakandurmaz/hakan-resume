79d7e88e1a8f658c983472f952ab630807cb1a8c
Hakan Durmaz <hakan@Hakans-MacBook-Pro.local>
2023-04-14 23:11:15 +0300

    updated webpage contents

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,30 +1,15 @@
 import {
   AcademicCapIcon,
   CalendarIcon,
-  DownloadIcon,
-  FlagIcon,
   MapIcon,
-  OfficeBuildingIcon,
   SparklesIcon,
 } from '@heroicons/react/outline';
 
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
-import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import heroImage from '../images/sameer-srivastava-M9kwG0fat3o-unsplash.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -44,8 +29,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Hakan Durmaz',
+  description: "Hakan Durmaz",
 };
 
 /**
@@ -69,28 +54,19 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Hi, I'm Hakan Durmaz.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a <strong className="text-stone-100">Senior Software Engineer</strong> at <strong className="text-stone-100">Numeris Labs</strong> specializing in building decentralized products
+        that support thriving ecosystems and empower users with innovative solutions.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        Please feel free to contact me at following social channels:
       </p>
     </>
   ),
   actions: [
-    {
-      href: '/assets/resume.pdf',
-      text: 'Resume',
-      primary: true,
-      Icon: DownloadIcon,
-    },
     {
       href: `#${SectionId.Contact}`,
       text: 'Contact',
@@ -104,16 +80,16 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description:
+    `Hello, I'm Hakan Durmaz, a passionate senior software developer with 10+ years of experience building innovative and high-performance software solutions.
+   I am always looking to learn and explore new technologies to stay ahead of the curve. 
+   Throughout my career, I have worked on projects of all sizes, from small applications to large-scale enterprise systems.
+  Let's connect and see how I can help your organization grow.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    { label: 'Location', text: 'Turkey', Icon: MapIcon },
+    { label: 'Motto', text: 'This is the way!', Icon: CalendarIcon },
+    { label: 'Interests', text: 'Science, Photography, Nature', Icon: SparklesIcon },
+    { label: 'Study', text: 'Computer Engineering', Icon: AcademicCapIcon },
   ],
 };
 
@@ -121,146 +97,14 @@ export const aboutData: About = {
  * Skills section
  */
 export const skills: SkillGroup[] = [
-  {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
-    ],
-  },
-  {
-    name: 'Frontend development',
-    skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
-      },
-    ],
-  },
-  {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
-      },
-    ],
-  },
+
 ];
 
 /**
  * Portfolio section
  */
 export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage11,
-  },
+
 ];
 
 /**
@@ -268,40 +112,107 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2022 - Ongoing',
+    location: 'Akdeniz University',
+    title: 'Master in Computer Engineering',
+    content: <p>Master's degree in Computer Science</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2010 - 2014',
+    location: 'Pamukkale University',
+    title: 'Computer Engineering',
+    content: <>
+      <p>Bachelor degree in Computer Engineering.</p>
+      <p>Activities and societies: Photography Society, Computer Society, Free Software Community</p>
+    </>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'May 2021 - Present',
+    location: 'Numeris Labs',
+    title: 'Founder',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        ➤ I founded my own company to accomplish my own projects.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'Aug 2021 - Dec 2021 (5 mos)',
+    location: 'Anadolu Sigorta',
+    title: 'Senior Software Developer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        ➤ Worked as a senior software developer consultant with my own company.
       </p>
+    ),
+  },
+  {
+    date: 'Sep 2016 - Aug 2021 (5 yrs)',
+    location: 'pribas Turkey Ltd',
+    title: 'Senior Software Developer',
+    content: (
+      <>
+        <p>
+          ➤ Developing spring boot, mongodb, extjs applications.
+        </p>
+        <p>
+          ➤ Hospitality-Solutions for global operating Tour-Operators (Dynamic Production & Dynamic Packaging Platforms)
+        </p>
+        <p>
+          ➤ Developing hotel & flight microservices in order to make reservations
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'Jul 2014 - Aug 2016 (2 yrs 2 mos)',
+    location: 'Hotelchk',
+    title: 'Mobile Software Developer',
+    content: (
+      <>
+        <p>
+          ➤ Developed Android + Windows Phone interactive applications which is guest communication & hospitality for tourism industry.
+        </p>
+        <p>
+          ➤ Developed Android & Windows Phone applications for Smart House solutions
+        </p>
+        <p>
+          ➤ Developed & Responsible for Kiosk applications that works on Android x86 platform.
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'Aug 2012 - July 2014 (2 yrs)',
+    location: 'Ratel Bilisim Ltd',
+    title: 'Software Developer',
+    content: (
+      <>
+        <p>
+          ➤ Developed Android applications for many leading companies
+        </p>
+        <p>
+          ➤ Developed Java (Spring, Hibernate, Primefaces) applications for Ahmet Yesevi University Information Systems
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'Jun 2012 - Aug 2012 (3 mos)',
+    location: 'Pamukkale University IT Department',
+    title: 'Intern Software Developer',
+    content: (
+      <>
+        <p>
+          ➤ Worked at Pusula Information System (Pamukkale University Information System)
+        </p>
+        <p>
+          ➤ Developed Asp.NET modules for building & optimizing Personnel Information System modules.
+        </p>
+      </>
     ),
   },
 ];
@@ -312,21 +223,7 @@ export const experience: TimelineItem[] = [
 export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
-    {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-    },
-    {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-    },
-    {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-    },
+
   ],
 };
 
@@ -340,23 +237,13 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: 'hello@hakandurmaz.com.tr',
+      href: 'mailto:hello@hakandurmaz.com.tr',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'hakandurmaz',
+      href: 'https://github.com/hakandurmaz',
     },
   ],
 };
@@ -365,9 +252,8 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/hakandurmaz' },
+  { label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/hakan-durmaz-tr/' },
+  { label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/hakandurmaz.tr/' },
+  { label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/0xhakandurmaz' },
 ];
